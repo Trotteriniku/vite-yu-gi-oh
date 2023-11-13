821cc94fc4957ef5f235dbbc3b065d579e36c31b
Trotteriniku <stefanozidda3@gmail.com>
2023-11-13 16:27:35 +0100

    scaffolding

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        <h1>Yu-Gi-Oh Api</h1>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent'
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
